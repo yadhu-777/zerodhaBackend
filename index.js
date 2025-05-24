f93b027c5322725d70c65226fe7dcb63c07ee250
yadhu krishna <yadhukris>
2025-05-24 12:20:56 +0530

    backi

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+require("dotenv").config()
 const express = require("express");
 const app  = express();
 const { Login } =require ("./controller/authlogin");
 const { Signup } =require ("./controller/authsign");
 const mongoose = require("mongoose");
 const { holdings } = require("./Models/HoldingsModel");
-require("dotenv").config()
+
 const url =process.env.Mongo_url 
 const { positions } = require("./Models/Positionmodel");
 const cors = require("cors")
-const bodyparser = require("body-parser")
+
 const {order}= require ("./Models/OrderModel");
-const {user} = require("./Models/Usermodel");
+
 const {VerifyUser} = require("./iddleware/verify");
 const cookieParser = require("cookie-parser");
-const Port = process.env.Port ;
+const Port = process.env.Port || 3000;
 const link1 = process.env.Link1;
 const link2 = process.env.Link2;
 app.listen(Port,()=>{
@@ -29,7 +30,7 @@ const connect =()=>mongoose.connect(url);
  })
  const allowedorigin = [link1,
   link2
- ]
+ ];
  app.use(cors({ origin: allowedorigin, credentials: true }));
  app.use(cookieParser());
  app.use(express.json());
